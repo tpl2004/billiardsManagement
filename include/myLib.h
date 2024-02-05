aebf0bb848414ed73f730a1ac6164e4b36a7822e
Truong Van Tan Tai <truongtai072424@gmail.com>
2024-02-05 23:58:34 +0700

    create: My Library

---
+++ include/myLib.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+/*kiem tra tinh hop le cua id*/
+bool checkID(std::string id);
+
+/*kiem tra tinh hop le cua ho ten*/
+bool checkFullName(std::string fullName);
+
+/*chuan hoa*/
+std::string standardize(std::string str);
---
+++ src/myLib.cpp
@@ -0,0 +1,37 @@
+#include "../include/myLib.h"
+#include <algorithm>
+#include <cctype>
+#include <sstream>
+#include <vector>
+
+bool checkID(const std::string &str) {
+    for(const char &c : str) {
+        if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) continue;
+        return false;
+    }
+    return true;
+}
+
+bool checkFullName(const std::string &fullName) {
+    for(const char &c : fullName) {
+        if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == ' ') continue;
+        return false;
+    }
+    return true;
+}
+
+std::string standardize(std::string fullName) {
+    transform(fullName.begin(), fullName.end(), fullName.begin(), ::tolower);
+    std::stringstream ss(fullName);
+    std::string tmp;
+    std::vector<std::string> v;
+    std::string newFullName = "";
+    while(ss >> tmp) {
+        tmp[0] = std::toupper(tmp[0]);
+        v.push_back(tmp);
+    }
+    for(int i = 0; i < int(v.size()) - 1; i++) {
+        newFullName += v[i] + " ";
+    }
+    return newFullName + v[int(v.size()) - 1];
+}
